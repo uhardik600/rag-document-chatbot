4ad31805e93ed2324648306314f8863503aab12f
Hardik upadhyay <hardikupadhyay600@gmail.com>
2026-05-27 17:24:53 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 import os
 from langchain_openai import ChatOpenAI
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from dotenv import load_dotenv
 
 load_dotenv()
