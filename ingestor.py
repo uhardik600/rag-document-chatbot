1fd2ad7b66947f03f4f1376c45157e3ac9891af6
Hardik upadhyay <hardikupadhyay600@gmail.com>
2026-05-27 17:25:34 +0530

    Update ingestor.py

--- ingestor.py
+++ ingestor.py
@@ -1,7 +1,7 @@
 import os
 from typing import List
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from embeddings import embed_and_store
 
 
